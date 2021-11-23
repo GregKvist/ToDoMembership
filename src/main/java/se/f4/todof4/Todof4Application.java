ab4e57097f55bd1704e4752542a59c1bf4f88df4
Greger Lindkvist <greg.kvist@gmail.com>
2021-11-23 10:31:03 +0100

    Added simple GetMapping

--- src/main/java/se/f4/todof4/Todof4Application.java
+++ src/main/java/se/f4/todof4/Todof4Application.java
@@ -2,12 +2,20 @@ package se.f4.todof4;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
 
 @SpringBootApplication
+@RestController
 public class Todof4Application {
 
     public static void main(String[] args) {
         System.out.println("main is called");
         SpringApplication.run(Todof4Application.class, args);
+    }
+
+    @GetMapping("/hello")
+    public	String sayHello(@RequestParam(value = "myName", defaultValue = "GetMapping") String name){
+        return String.format("Hello %s!", name);
+
     }
 }
