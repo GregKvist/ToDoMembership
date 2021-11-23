3178f55adac63898c099558e57c9982bde8d55e7
Greger Lindkvist <greg.kvist@gmail.com>
2021-11-23 14:54:14 +0100

    Removed Account, compileable ToDof4Application

--- src/main/java/se/f4/todof4/Todof4Application.java
+++ src/main/java/se/f4/todof4/Todof4Application.java
@@ -2,6 +2,7 @@ package se.f4.todof4;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @SpringBootApplication
