a2ec0d2ea72904e9fc10561d3e3e4fd82a657e8e
Jacob Bolghari <j-bolghari@live.se>
2021-11-23 14:21:08 +0100

    Pojo-entity. JPA-mysql koppling, get & post- metoder & enpoints.
    Skapat pojo-entity för en User. Kopplat Jpa till entity'n. Börjat med att skapa metoder för get och post users (service) samt endpoints till dessa (controller) som genom JPA läggs in till den kopplade lokala MySQL.

---
+++ src/main/java/se/f4/todof4/controller/UserController.java
@@ -0,0 +1,29 @@
+package se.f4.todof4.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import se.f4.todof4.entity.User;
+import se.f4.todof4.service.UserService;
+
+import java.util.List;
+
+@RestController
+public class UserController {
+
+    @Autowired
+    private UserService service;
+
+    @PostMapping("/signup")
+    public User signUp(@RequestBody User user) {return service.createUser(user);}
+
+
+    @GetMapping("/users")
+    public @ResponseBody ResponseEntity<List> getUsers() {
+        return new ResponseEntity<List>(service.getUsers(), HttpStatus.OK);
+    }
+
+
+
+}
