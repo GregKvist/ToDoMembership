f5d46113eca7cd4d2826badd2a42b16e80824d1d
CicciCarboo <72078492+CicciCarboo@users.noreply.github.com>
2021-11-30 14:23:47 +0100

    index.html: Headers and paragrafs, UserController: removed old update method

--- src/main/java/se/f4/todof4/service/UserService.java
+++ src/main/java/se/f4/todof4/service/UserService.java
@@ -55,32 +55,6 @@ public class UserService {
         return isFound;
     }
 
-//    Enklaste updateUser utan HTTP status, låt stå/Cicci
-//    public String updateUser(User user){
-//        // Check first if user exist, if yes: change userFound to "true" and update user.
-//        // If not, userFound will stay "false" and trigger an error message
-//        boolean userFound = false;
-//
-//        for (User currentUser: getUsers()) {
-//            if(currentUser.getId() == user.getId()){
-//                userFound = true;
-//                currentUser.setName(user.getName());
-//                currentUser.setEmail(user.getEmail());
-//                currentUser.setPassword(user.getPassword());
-//
-//                // Invoke create user to write the updates to database
-//                createUser(currentUser);
-//            }
-//        }
-//
-//        // Check final status of userFound
-//        if(!userFound){
-//            return "No such user exist, check entered id number.";
-//        }else {
-//            return "User updated successfully.";
-//        }
-//    }
-
     //update user by ID
     @Transactional
     public boolean updateUser(Integer id, String name , String email, String password) {
